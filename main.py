82badc3b74fb37f4942f4d2b4c454c6b67a30818
niaki <nina.zizakic@ugent.be>
2022-02-18 11:04:18 +0100

    Copy files from local-img-descr-ae repo

---
+++ main.py
@@ -0,0 +1,43 @@
+import wandb
+import os
+from pathlib import Path
+
+import utilities
+from unite_sweeps_into_one import sweep_one_sweep_to_rule_them_all
+
+if __name__ == '__main__':
+    sweep_config = {
+        'method': 'grid',
+        'project': 'evaluate-ae',
+        'metric': {
+            'goal': 'minimize',
+            'name': 'hpatches_overall'
+        },
+        'parameters': {
+            'data_augm_level': {
+                'values': [0, 1, 2, 3]
+            },
+            'activation_fn': {
+                'values': ['elu', 'relu']
+            },
+            'loss_fn': {
+                'values': ['bce', 'msssim']
+            },
+            'vae_beta_norm': {
+                'values': [0.01, 0.001, 0.0001, 0.00001, 0]  # 0 means it's AE
+            },
+            'learning_rate': {
+                'values': [0.0001]
+            }
+        }
+    }
+
+    sweep_version = 'sweep__one_sweep_to_rule_them_all_v2022_0'  # TODO change in both files!!! (TODO make it a parameter)
+    sweep_dir = os.path.join('/scratch/image_datasets/3_65x65/ready/weights', sweep_version)
+    Path(sweep_dir).mkdir(parents=True, exist_ok=True)
+
+    # Set the logger
+    utilities.set_logger(os.path.join(sweep_dir, 'train.log'))
+
+    sweep_id = wandb.sweep(sweep_config, project="evaluate-ae")  # TODO!!!
+    wandb.agent(sweep_id, function=sweep_one_sweep_to_rule_them_all)
---
+++ unite_sweeps_into_one.py
@@ -0,0 +1,85 @@
+"""Unite different W+B sweeps into one W+B sweep for easier data manipulation and visualisation"""
+
+import argparse
+import logging
+import os
+from pathlib import Path
+import pandas as pd
+import wandb
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--weights_dir', default='/scratch/image_datasets/3_65x65/ready/weights',
+                    help="Directory where weights will be saved")
+
+
+def load_sweep_csv(filepath):
+
+    # I made this csv file by downloading csv files from different wandb sweeps and uniting them all into one csv file
+    df = pd.read_csv(filepath)
+
+    # replace . with _ in the column names so that pandas doesn't rename the columns
+    new_columns = df.columns.values
+    for i, column in enumerate(new_columns):
+        new_column = column.replace('.', '_')
+        new_columns[i] = new_column
+    df.columns = new_columns
+    return df
+
+
+sweep_df = load_sweep_csv('sweep_results.csv')
+
+
+def find_row_with_inputs(activation_fn, data_augm_level, loss_fn, vae_beta_norm):
+    for row in sweep_df.itertuples():
+        if row.activation_fn == activation_fn and row.data_augm_level == data_augm_level and \
+                 row.loss_fn == loss_fn and row.vae_beta_norm == vae_beta_norm:
+            print(row.Name)
+            return row
+
+
+def sweep_one_sweep_to_rule_them_all():
+
+    args = parser.parse_args()
+
+    use_wandb = False
+
+    if use_wandb:
+        wandb_run = wandb.init()
+
+    logging.info("\n\n****************** STARTING A NEW RUN ******************")
+    logging.info('Data augmentation level: ' + str(wandb.config.data_augm_level))
+    logging.info('Activation function    : ' + str(wandb.config.activation_fn))
+    logging.info('Loss function          : ' + str(wandb.config.loss_fn))
+    logging.info('Beta value (normalised): ' + str(wandb.config.vae_beta_norm))
+    logging.info("")
+
+    latent_size = 32
+    batch_size = 32
+    logging.info('Other params (that are not being swept)')
+    logging.info('    Latent size:' + str(latent_size))
+    logging.info('    Batch size :' + str(batch_size))
+    logging.info("")
+
+    wandb.config.variational = wandb.config.vae_beta_norm > 0.0000001
+    wandb.config.latent_size = latent_size
+    wandb.config.batch_size = batch_size
+    wandb.config.num_workers = 4
+    wandb.config.vae_or_ae = "vae" if wandb.config.vae_beta_norm > 0.0000001 else "ae"
+
+    sweep_version = 'sweep__one_sweep_to_rule_them_all_v2022_0'  # TODO change in both files!!! (TODO make it a parameter)
+
+    Path(os.path.join(args.weights_dir, sweep_version)).mkdir(parents=True, exist_ok=True)
+
+    # find and read the entry from the data frame
+    row_df = find_row_with_inputs(wandb.config.activation_fn, wandb.config.data_augm_level, wandb.config.loss_fn, wandb.config.vae_beta_norm)
+
+    if use_wandb:
+        wandb.log({"num_epochs": row_df.num_epochs, "variational": row_df.variational,
+                   "hpatches_overall": row_df.hpatches_overall,
+                   "matching_overall": row_df.matching_overall,
+                   "retrieval_overall": row_df.retrieval_overall,
+                   "verification_overall": row_df.verification_overall,
+                   "mse": row_df.mse, "psnr": row_df.psnr, "ssim": row_df.ssim, "loss": row_df.loss})
+
+    if use_wandb:
+        wandb_run.finish()
